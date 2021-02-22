7b2112149e3779e33f8868b654565b4bf7c972d2
Arashiii21 <71123444+Arashiii21@users.noreply.github.com>
2021-02-22 23:53:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // all objects storing all expenses properties are stored here
+var table11 = document.getElementById("table11");
+var selectPeriod = document.getElementById("selectperiod");
+table11.value = localStorage.getItem("table11");
+selectPeriod.value = localStorage.getItem("selectperiod"); 
 var profit = [];
 
 var o = "";
@@ -206,5 +210,21 @@ function showExpense()
     expenseSumCell.innerHTML         = "₱" + sumExpenses;
 }
 
+var table11 = document.getElementById("table11");
+var selectPeriod = document.getElementById("selectperiod");
+try {
+    localStorage.setItem("table11", table11.value);
+    localStorage.setItem("age", selectPeriod.value);
+    table11.value = "";
+    selectPeriod.value = "";
+} 
+catch (e) {
+    if (e == QUOTA_EXCEEDED_ERR) {
+        console.log("Error: Local Storage limit exceeds.");
+    }
+    else {
+        console.log("Error: Saving to local storage.");
+    }
+} 
 
 
